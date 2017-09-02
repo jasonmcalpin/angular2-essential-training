39f087b66c9ab49ccd8f7189a12058daaa6344bd
Jason McAlpin <jason@chaiboy.com>
2017-09-02 13:06:29 -0400

    new styles rock

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,29 @@
+/**
+ * Starting component.
+ * This is the component that is first run based on bootstrap in the main module. 
+ * 
+ */
+
 import { Component } from '@angular/core';
 
 /**
  * import component from core.
  * 
  * The it is defined. It requires two things.
- * selector: the element on the html page that will be filled with the template.
- * template: the html code or actual template that will be used to fill selector
+ * selector: the element name on the html page that will be filled with the template.
+ * template: the html code or actual template that will be used to fill selector.
  * 
  * export class: is used to expose a class to the rest of the app.
+ * can use back ticks to allow multi-line values that won't break because of line returns.
+ * 
  */
 @Component({
-  selector: 'app',
-  template: '<h1>My app</h1>'
+  selector: 'mw-app',
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css']
 })
-export class AppComponent {}
+export class AppComponent { }
+
+/**
+ * should use dashes in new element names and be prefixed with name of app
+ */
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,3 +1,7 @@
+/**
+ * Root module
+ */
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
@@ -13,4 +17,4 @@ import { AppComponent } from './app.component';
     AppComponent
   ]
 })
-export class AppModule {}
+export class AppModule { }
